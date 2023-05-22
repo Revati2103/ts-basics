f37841dba6f486d21179b81709eb16cf703fcfa4
Revati <revati.v.bordawekar@gmail.com>
2023-05-22 06:21:05 -0700

    basic types

--- basics.ts
+++ basics.ts
@@ -1,5 +1,35 @@
-let username = 'Revati';
-let hasLoggedIn = true;
+let username : string = 'Revati';
+let hasLoggedIn: boolean = true;
 
-hasLoggedIn += 'B';
-console.log(hasLoggedIn);
+username += 'B';
+console.log(username);
+
+const names: string[] = username.split(' ');
+const myValues: Array<number> = [1,2,3];
+
+const myPerson: {
+    first: string;
+    last: string;
+} = {
+    first: 'Reva',
+    last: 'B'
+}
+
+//OR
+
+interface Person {
+    first: string;
+    last: string;
+}
+
+const myPersonOne: Person = {
+    first: 'Reva',
+    last: 'B'
+}
+
+//for map 
+
+const ids: Record<number, string> = {
+    10: "a",
+    20: "b"
+}
